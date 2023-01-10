756a665109d05a68186f949af733226df227ce07
Mauro Araujo <e.mauroar@gmail.com>
2023-01-10 16:52:19 -0300

    navbar added to settigs screen

--- app/activities/settings/settings.tsx
+++ app/activities/settings/settings.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import DefaultLayout from '../../layout/default/default';
+import NavigationBar from '../../components/navigationBar/navigationBar';
 
 export default function Settings() {
   return (
     <DefaultLayout>
+      <NavigationBar name="Settings" />
+
       <View>
         <Text>Settings</Text>
       </View>
