ef35dc36dc43d0574b6c19ef481dd3189c1b69c0
Mauro Araujo <e.mauroar@gmail.com>
2023-01-10 16:32:24 -0300

    password card adjustments

--- app/components/passwordElement/passwordElement.styles.tsx
+++ app/components/passwordElement/passwordElement.styles.tsx
@@ -1,7 +1,13 @@
 import {StyleSheet} from 'react-native';
+import {Colors, Layout} from '../../res';
 
 export default StyleSheet.create({
   container: {
-
+    backgroundColor: Colors.Light.CardColor,
+    marginTop: Layout.Spacing.SM,
+    elevation: 10,
+    shadowColor: Colors.Light.ShadowColor,
+    borderRadius: Layout.BorderRadius.SM,
+    padding: Layout.Spacing.SM,
   },
 });
